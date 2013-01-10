ad7724c9d302702fe343c88ff4c6578e432ab9da
Jerimiah Ham <jerimiah797@gmail.com>
2013-01-09 16:31:57 -0800

    Refactor song object

--- juke.rb
+++ juke.rb
@@ -19,7 +19,7 @@ $developerKey = "4B8C5B7B5B7B5I4H"
 $cobrandId = "40134"
 $client_type = "sonos"
 $localhost = "127.0.0.1"
-$track_id = "Tra.67752792"
+
 
 class Member
   @logged_in = false
@@ -54,15 +54,24 @@ class Member
       puts "Login Error: "+@auth_hash["localizedMessage"]
     end
   end
-  def get_track
-    get_track_url = "https://playback.rhapsody.com/getContent.json?token=#{encode(@token)}&trackId=#{$track_id}&pcode=rn&nimdax=true&mid=123"    
-    @track_hash = JSON.parse(RestClient.get(get_track_url))
+  def get_token
+    @token
+  end
+end
+class Song
+  def initialize( token, song_id )
+    @token = token
+    @song_id = song_id
+  end
+  def get_track_mediaurl
+    url = "https://playback.rhapsody.com/getContent.json?token=#{encode(@token)}&trackId=#{@song_id}&pcode=rn&nimdax=true&mid=123"    
+    @track_hash = JSON.parse(RestClient.get(url))
     @mediaUrl = @track_hash["data"]["mediaUrl"]
     @playbackSessionId = @track_hash["data"]["playbackSessionId"]
   end
   def get_track_metadata
-    track_metadata_url = "http://direct.rhapsody.com/metadata/data/methods/getLiteTrack.js?developerKey=4B8C5B7B5B7B5I4H&cobrandId=40134&filterRightsKey=0&trackId=#{$track_id}"
-    @metadata_hash = JSON.parse(RestClient.get(track_metadata_url))
+    url = "http://direct.rhapsody.com/metadata/data/methods/getLiteTrack.js?developerKey=4B8C5B7B5B7B5I4H&cobrandId=40134&filterRightsKey=0&trackId=#{@song_id}"
+    @metadata_hash = JSON.parse(RestClient.get(url))
   end
   def put_track_metadata
     Mp3Info.open("testfile3.mp3") do |mp3|
@@ -76,22 +85,25 @@ class Member
     platform = "WIN 11,4,402,287"
     mpswf = "http://www.rhapsody.com/assets/flash/MiniPlayer.swf"
     filename = "testfile3.flv"
-
     uri = Addressable::URI.parse(@mediaUrl)
-
     pathsub1 = uri.path[1..8]
     pathsub2 = uri.path[10..-1]
-
-
     seg1 = "#{uri.scheme}://#{uri.host}/#{pathsub1}"
     seg2 = pathsub1
     seg3 = "mp3:#{pathsub2}?#{uri.query}"
-
-
     cmdstring = %Q(rtmpdump -r "#{seg1}" -a "#{seg2}" -f "#{platform}" -W "#{mpswf}" -y "#{seg3}" -o "#{filename}")
-    #puts cmdstring
-    
     system cmdstring
+  end
+  def strip_mp3
+    movie = FFMPEG::Movie.new("testfile3.flv")
+    transcoded_audio = movie.transcode("testfile3.mp3", "-vn -acodec copy")
+  end
+  def process
+    get_track_mediaurl
+    get_track_metadata
+    fetch_flv
+    strip_mp3
+    put_track_metadata
   end
 end
 
@@ -99,34 +111,30 @@ def encode(thing)
   URI.escape(thing,Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
 end
 
-def strip_mp3
-  movie = FFMPEG::Movie.new("testfile3.flv")
-  transcoded_audio = movie.transcode("testfile3.mp3", "-vn -acodec copy")
-end
   
 def play_mp3
   the_command = %Q(/Applications/VLC.app/Contents/MacOS/VLC -I rc testfile3.mp3)
   system the_command
 end
 
-def initializeUser
+def initializeUser (user)
   puts 'Welcome to Juke, the Rhapsody shell client'
   print 'Enter your username: '
-  @user.set_logon(gets().chomp)
+  user.set_logon(gets().chomp)
   print 'Enter your password: '
-  @user.set_password(ask("") { |q| q.echo = false })
-  @user.login_member()
+  user.set_password(ask("") { |q| q.echo = false })
+  user.login_member()
 end
 
+song_id = "Tra.67752792"
+user = Member.new
+initializeUser(user)
+puts user.get_logon
+puts user.get_password
+song = Song.new(user.get_token, song_id)
+song.process
 
-@user = Member.new
-#initializeUser
-#@user.get_track
-@user.get_track_metadata
-#@user.fetch_flv
-#strip_mp3
-@user.put_track_metadata
-#play_mp3
+play_mp3
 
 
 
